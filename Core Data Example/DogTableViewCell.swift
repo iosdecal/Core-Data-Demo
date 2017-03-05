836c54b8d93fffbe29425730f85e32b2e2214f80
Paige Plander <paigeplan@berkeley.edu>
2017-03-04 18:46:42 -0800

    first commit

---
+++ Core Data Example/DogTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DogTableViewCell.swift
+//  Core Data Example
+//
+//  Created by Paige Plander on 3/4/17.
+//  Copyright © 2017 Paige Plander. All rights reserved.
+//
+
+import UIKit
+
+class DogTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
